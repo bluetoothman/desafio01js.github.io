707ee7af74fc98498df8bfd36169a9fd085dc9b5
bluetoothman <76394029+bluetoothman@users.noreply.github.com>
2021-10-05 11:41:59 -0500

    commit1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Archivo Javascript
+
+const PI = 3.1416
+
+let nombre = prompt('Ingresa tu nombre');
+let apellido = prompt("y tu apellido");
+
+datos = nombre + apellido;
+let radio = prompt("Para calcular el area de un circulo. Ingresa su radio: ");
+
+console.log("Señor ", datos + ".\n" + "El area del circulo es (pi)*r^2= ", PI * radio * radio)
+alert("Señor " + datos + ".\n" + "El area del circulo es (pi)*r^2= " + PI * radio * radio)
